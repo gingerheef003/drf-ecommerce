40ac2e86481511768fa47c655c0db4a801e1321d
Ginger Heef <gingerheef003@gmail.com>
2023-03-07 16:50:26 +0530

    Added Unit tests

---
+++ drfecommerce/drfecommerce/tests/product/test_models.py
@@ -0,0 +1,29 @@
+import pytest
+pytestmark = pytest.mark.django_db
+
+
+class TestCategoryModel:
+    def test_str_method(self, category_factory):
+        #Arrange
+        #Act
+        x = category_factory(name="test_cat")
+        #Assert
+        assert x.__str__() == "test_cat"
+
+
+class TestBrandModel:
+    def test_str_method(self, brand_factory):
+        #Arrange
+        #Act
+        x = brand_factory(name="test_br")
+        #Assert
+        assert x.__str__() == "test_br"
+
+
+class TestProductModel:
+    def test_str_method(self, product_factory):
+        #Arrange
+        #Act
+        x = product_factory(name="test_pr")
+        #Assert
+        assert x.__str__() == "test_pr"
--- drfecommerce/drfecommerce/tests/test_example.py
+++
@@ -1,2 +0,0 @@
-def test_exampe():
-    assert 1==1
