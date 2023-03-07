f30066430d01442601d6091e67d60a43850af3d3
Ginger Heef <gingerheef003@gmail.com>
2023-03-07 15:52:54 +0530

    Spectacular for all

--- drfecommerce/drfecommerce/product/views.py
+++ drfecommerce/drfecommerce/product/views.py
@@ -3,8 +3,8 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from drf_spectacular.utils import extend_schema
 
-from .models import Category
-from .serializers import CategorySerializer
+from .models import Brand, Category, Product
+from .serializers import BrandSerializer, CategorySerializer, ProductSerializer
 
 class CategoryViewSet(viewsets.ViewSet):
     """
@@ -16,4 +16,28 @@ class CategoryViewSet(viewsets.ViewSet):
     @extend_schema(responses=CategorySerializer)
     def list(self,request):
         serializer = CategorySerializer(self.queryset, many=True)
+        return Response(serializer.data)
+
+class BrandViewSet(viewsets.ViewSet):
+    """
+    A simple vieset for brand
+    """
+
+    queryset = Brand.objects.all()
+
+    @extend_schema(responses=BrandSerializer)
+    def list(self,request):
+        serializer = BrandSerializer(self.queryset, many=True)
+        return Response(serializer.data)
+
+class ProductViewSet(viewsets.ViewSet):
+    """
+    A simple vieset for brand
+    """
+
+    queryset = Product.objects.all()
+
+    @extend_schema(responses=ProductSerializer)
+    def list(self,request):
+        serializer = ProductSerializer(self.queryset, many=True)
         return Response(serializer.data)
