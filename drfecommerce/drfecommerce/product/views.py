f1509b85d5caf23907f765ee17b45fb6f307139d
Ginger Heef <gingerheef003@gmail.com>
2023-03-07 15:37:31 +0530

    Added spectacular with products

---
+++ drfecommerce/drfecommerce/product/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from drf_spectacular.utils import extend_schema
+
+from .models import Category
+from .serializers import CategorySerializer
+
+class CategoryViewSet(viewsets.ViewSet):
+    """
+    A simple vieset for category
+    """
+
+    queryset = Category.objects.all()
+
+    @extend_schema(responses=CategorySerializer)
+    def list(self,request):
+        serializer = CategorySerializer(self.queryset, many=True)
+        return Response(serializer.data)
--- drfecommerce/drfecommerce/urls.py
+++ drfecommerce/drfecommerce/urls.py
@@ -10,12 +10,23 @@ Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
+# Create your views here.
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
+from drfecommerce.product import views
+
+router = DefaultRouter()
+router.register(r"category", views.CategoryViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+    path('api/schema/', SpectacularAPIView.as_view(), name="schema"),
+    path('api/schema/docs/', SpectacularSwaggerView.as_view(url_name="schema")),
 ]
